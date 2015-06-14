3a2134204539f99b9cc84f303a674003478a8ae3
Aeyos <vinicius.m.negrao@hotmail.com>
2015-06-14 18:37:12 -0300

    gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var gulp = require('gulp'),
+		concat = require('gulp-concat'),
+		connect = require('gulp-connect'),
+		del = require('del'),
+		imagemin = require('gulp-imagemin'),
+		less = require('gulp-less'),
+		livereload = require('gulp-livereload'),
+		sourcemaps = require('gulp-sourcemaps'),
+		uglify = require('gulp-uglify');
+
+var paths = {
+	scripts: './app/scripts/*.js',
+	images: './app/images/*',
+	styles: './app/styles/*.less',
+	index: './app/*.html'
+};
+
+// TASKS
+
+gulp.task('clean', function(cb) {
+	del(['build'], cb);
+});
+
+gulp.task('connect', function() {
+	connect.server({
+		root: 'app',
+		livereload: true
+	});
+});
+ 
+gulp.task('html', function () {
+	return gulp.src(paths.index)
+		.pipe(connect.reload());
+});
+
+gulp.task('images', ['clean'], function() {
+	return gulp.src(paths.images)
+		// Pass in options to the task
+		.pipe(imagemin({optimizationLevel: 5}))
+		.pipe(gulp.dest('build/img'));
+});
+
+gulp.task('less', function() {
+	return gulp.src(paths.styles)
+		.pipe(less())
+		.pipe(gulp.dest('css'))
+		.pipe(livereload());
+});
+
+gulp.task('scripts', ['clean'], function() {
+	return gulp.src(paths.scripts)
+		.pipe(sourcemaps.init())
+			.pipe(uglify())
+			.pipe(concat('all.min.js'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('build/js'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch(paths.scripts, ['scripts']);
+	gulp.watch(paths.images, ['images']);
+	gulp.watch(paths.styles, ['less']);
+	gulp.watch(paths.index, ['html']);
+});
+ 
+gulp.task('default', ['connect', 'watch', 'scripts', 'images', 'less', 'html']);
